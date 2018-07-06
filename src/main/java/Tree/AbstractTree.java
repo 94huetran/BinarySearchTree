6443eb4c65ee202509d274a00fa0247ca0e27715
Tran Thu Hue <94huetran@gmail.com>
2018-07-06 11:34:35 +0700

    update

--- src/main/java/Tree/AbstractTree.java
+++ src/main/java/Tree/AbstractTree.java
@@ -3,9 +3,8 @@ package Tree;
 public abstract class AbstractTree<E> implements Tree<E> {
 
     @Override
-    public String inorder() {
+    public void inorder() {
 
-        return null;
     }
 
 }
