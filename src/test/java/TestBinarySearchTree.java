4d4e2ee37393fe33a04aa98218c22664b1e17742
Tran Thu Hue <94huetran@gmail.com>
2018-07-06 11:17:56 +0700

    add TestBinarySearchTree

---
+++ src/test/java/TestBinarySearchTree.java
@@ -0,0 +1,18 @@
+import Tree.BinarySearchTree;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestBinarySearchTree {
+    BinarySearchTree<String> tree = new BinarySearchTree<String>();
+
+    @Test
+    public void TestInsert() {
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Mark");
+        tree.insert("Jones");
+        tree.insert("Harry");
+        int sort = tree.getSize();
+        Assert.assertEquals(5, sort);
+    }
+}
